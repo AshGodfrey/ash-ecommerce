0b84602fe770fa7d0c8274a6ac1d587592cac08a
Ash Godfrey <ashgodfrey@Ashs-MacBook-Pro.local>
2023-09-25 23:50:04 -0600

    fixed an error that occured for products with no image

--- components/ProductPage/ProductImage.tsx
+++ components/ProductPage/ProductImage.tsx
@@ -9,13 +9,13 @@ export default function ProductImage({ image }: ProductImageProps) {
   return (
     <div className="md:w-1/3">
       <Image
-        src={image.url}
-        alt={image.altText}
+        src={image?.url || '/image-coming-soon.jpg'}
+        alt={image?.altText || "No image for this product."}
         width={500}
         height={500}
         className="rounded-lg shadow-2xl object-cover object-center transition-transform duration-500 hover:scale-105"
         placeholder="blur"
-        blurDataURL={image.url}
+        blurDataURL={image?.url || '/image-coming-soon.jpg'}
       />
     </div>
   );
